081a0437e7763f201dce264f93245f6f51369daa
Brandon Chao <bchao1@stuy.edu>
2019-02-07 11:37:54 -0500

    preliminary addqueen/remove

--- QueenBoard.java
+++ QueenBoard.java
@@ -14,5 +14,38 @@ public class QueenBoard{
 
   }
 
-  
+  private boolean addQueen(int r, int c) {
+    if (board[r][c] > 0) {
+      return false;
+    }
+    board[r][c] = -1;
+    return true;
+  }
+
+  private boolean removeQueen(int r, int c) {
+    if (board[r][c] != -1){
+      return false;
+    }
+    board[r][c] = 0; //this may have to change
+  }
+
+  public String toString(){
+    for (int r = 0; r < board.length;r++) {
+      for (int c = 0; c < board[r].length;c++){
+        
+      }
+    }
+  }
+
+  public boolean solve(){
+
+  }
+
+  public int countSolutions(){
+
+  }
+
+  private void addHorizontally(int[][] border) {
+
+  }
 }
